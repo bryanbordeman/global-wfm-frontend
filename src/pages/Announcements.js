04f0cd751696f9125657c15dbd7146fcebb20c84
Bryan Bordeman <bryanbordeman@Bryans-MacBook-Pro.local>
2022-05-25 06:23:56 -0400

    Added Announcement Service

--- src/pages/Announcements.js
+++ src/pages/Announcements.js
@@ -1,8 +1,60 @@
 import React from 'react';
 import LoginMessage from '../components/LoginMessage';
+import AnnouncementDataService from '../services/Announcement.services'
+import { Paper, Grid, ListItem, ListItemText, Container, Divider } from '@mui/material';
 
 function Announcements(props) {
     const { user } = props
+    const [ announcements, setAnnouncements ] = React.useState([])
+
+    React.useEffect(() => {
+        retrieveAnnouncements()
+    },[])
+
+    const retrieveAnnouncements = () => {
+        AnnouncementDataService.getAll(props.token)
+        .then(response => {
+            setAnnouncements(response.data);
+        })
+        .catch( e => {
+            console.log(e);
+        })
+    }
+
+    const announcementList = announcements.map(announcement => (
+        <Paper
+            sx={{
+            my: 1,
+            width: '100%',
+            maxWidth: '500px',
+            border: 0.5,
+            borderColor: 'primary.main',
+            borderRadius: '16px',
+            }}
+            variant="outlined"
+            key={announcement.id}
+        >
+            <Grid container wrap="nowrap" spacing={2}>
+            <Grid item xs zeroMinWidth>
+                <ListItem 
+                    key={announcement.id}
+                >
+                    <ListItemText
+                        key={announcement.id}
+                        primary={
+                        <div style={{fontWeight: '700', marginBottom: '.5rem'}}>
+                            {announcement.title}
+                            <Divider/>
+                        </div>
+                        }
+                        secondary={announcement.memo}
+                    />
+                </ListItem>
+            </Grid>
+            </Grid>
+        </Paper>
+    ))
+
     return ( 
         <div>
             {!user.username ? 
@@ -11,7 +63,16 @@ function Announcements(props) {
             </div>
             : 
             <div style={{paddingTop: '1rem'}}> 
-                Announcements
+                <Container
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flexDirection:'column',
+                        height: '100%'
+                    }}>
+                        {announcementList}
+                </Container>
             </div>
             }
         </div>
---
+++ src/services/Announcement.services.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const SERVER = 'http://localhost:8000'
+
+class AnnouncementDataService {
+    getAll(token){
+        axios.defaults.headers.common["Authorization"] = "Token " + token;
+        return axios.get(`${SERVER}/api/announcement/`);
+    };
+    // getWeek(token, isoweek){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.get(`${SERVER}/api/worksegments/${isoweek}/`);
+    // };
+    // adminGetWeek(token, isoweek){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.get(`${SERVER}/api/admin/worksegments/${isoweek}/`);
+    // };
+    // createWorksegment(data, token){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.post(`${SERVER}/api/create/worksegment/`, data);
+    // };
+    // adminCreateWorksegment(data, token, userId){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.post(`${SERVER}/api/create/worksegment/${userId}/`, data);
+    // };
+    // updateWorksegment(id, data, token){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.put(`${SERVER}/api/worksegment/${id}`, data);
+    // };
+    // deleteWorksegment(id, token){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.delete(`${SERVER}/api/worksegment/${id}`);
+    // };
+    // approveWorksegment(id, token){
+    //     axios.defaults.headers.common["Authorization"] = "Token " + token;
+    //     return axios.put(`${SERVER}/api/worksegment/${id}/approved/`);
+    // };
+}
+
+export default new AnnouncementDataService();
